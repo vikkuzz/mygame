f24f42e08768fb37e347a51692b1595fc4403036
Vikkuzz <simpat007@yandex.ru>
2025-03-29 00:52:35 +0300

    fix : test

--- public/js/game.js
+++ public/js/game.js
@@ -27,7 +27,7 @@ function preload() {
  
 function create() {
   var self = this;
-  this.socket = io("https://mygame-lac.vercel.app:8081");
+  this.socket = io();
   this.otherPlayers = this.physics.add.group();
   this.socket.on('currentPlayers', function (players) {
     Object.keys(players).forEach(function (id) {
